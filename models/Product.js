5ea9abe98f9460effb36a0e0ffbdbae6f33c2ac4
Ujjwal Paul <devujjwal1989@gmail.com>
2023-01-20 16:38:54 +0530

    uploading all files

---
+++ models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    cat_name: { type: String },
+    image: { type: String, required: true },
+    images: { type: [String] },
+    price: { type: Number, required: true },
+    price_2_year: { type: String },
+    brand: { type: String, required: true },
+    in_stock: { type: Number, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+    featuredImage: { type: String },
+    isFeatured: { type: Boolean, required: true, default: false },
+    color: { type: String },
+    sku: { type: String, required: true },
+    warehouse: { type: String },
+    exp_delivery: { type: Number },
+    delivery_date: { type: String },
+    exp_available: { type: Number },
+    ean: { type: String },
+    properties: {
+      full_name: { type: String },
+      ean: { type: String },
+      item_spec: { type: String },
+      warranty: { type: String },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product =
+  mongoose.models.Product || mongoose.model('Product', productSchema);
+export default Product;
---
+++ pages/api/cron.js
@@ -0,0 +1,66 @@
+import Product from '../../models/Product';
+import db from '../../utils/db';
+import axios from 'axios';
+
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+
+const handler = async (req, res) => {
+  try {
+    axios.defaults.baseURL = 'https://restful.mobileshop.bz';
+    axios.defaults.headers.common['Authorization'] = process.env.AUTH_KEY;
+
+    const response = await axios.get('/getStock', {
+      params: { lang_id: 0, price_drop: 0 },
+    });
+
+    if (response.data.stock.length > 0 && response.data.error === 0) {
+      const products = response.data.stock.map((item) => ({
+        id: item.id,
+        name: item.model,
+        slug: slugify(item.properties.full_name),
+        category: item.category,
+        cat_name: item.cat_name,
+        image: item.image,
+        images: item.images,
+        price: item.price,
+        price_2_year: item.price_2_year,
+        brand: item.name,
+        in_stock: item.in_stock,
+        countInStock: item.in_stock,
+        featuredImage: item.image,
+        isFeatured:
+          item.properties.item_spec == 'EU Spec' &&
+          (item.properties.warranty != 'No warranty' ||
+            item.properties.warranty != '')
+            ? true
+            : false,
+        color: item.color,
+        sku: item.sku,
+        warehouse: item.warehouse,
+        exp_delivery: item.exp_delivery,
+        delivery_date: item.delivery_date,
+        exp_available: item.exp_available,
+        ean: item.ean,
+        properties: item.properties,
+      }));
+      await db.connect();
+      await Product.deleteMany();
+      await Product.insertMany(products);
+      await db.disconnect();
+      res.send({ message: 'Products updated successfully' });
+    } else {
+      res.send({ message: 'Products not updated' });
+    }
+  } catch (error) {
+    console.log('Error', error);
+    res.send({ message: error });
+  }
+};
+
+export default handler;
