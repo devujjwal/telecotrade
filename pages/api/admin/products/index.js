b1d8fe2130c80dc690fa75c346b2b98a25181922
Ujjwal Paul <devujjwal1989@gmail.com>
2023-01-23 07:17:40 +0530

    cloudinary removed

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -18,18 +18,7 @@ const handler = async (req, res) => {
 };
 const postHandler = async (req, res) => {
   await db.connect();
-  const newProduct = new Product({
-    name: 'sample name',
-    slug: 'sample-name-' + Math.random(),
-    image: '/images/shirt1.jpg',
-    price: 0,
-    category: 'sample category',
-    brand: 'sample brand',
-    countInStock: 0,
-    description: 'sample description',
-    rating: 0,
-    numReviews: 0,
-  });
+  const newProduct = new Product({});
 
   const product = await newProduct.save();
   await db.disconnect();
